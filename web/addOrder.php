a8ddc515693086750eec1c322e6153e6a3289486
maxjamin <maxjamin@gmail.com>
2019-02-09 11:26:13 -0700

    add user button

--- web/addOrder.php
+++ web/addOrder.php
@@ -24,6 +24,34 @@
 	 	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 	  	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+
+	  		echo $sessionUser . ' ' . $adress . $zip . $state;
+
+		//check to see if the user_id matches the session id of the user
+		$stmt = $db->prepare('SELECT * FROM Customer WHERE userName=:sessionUser');
+		$stmt->bindValue(':sessionUser', $sessionUser, PDO::PARAM_STR);
+		$stmt->execute();
+		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+		//Add the user_id into the Order table
+		if($rows[0]["username"])
+		{
+			echo $rows[0]["username"] ." " . $rows[0]["user_id"];
+			$userId = $rows[0]["user_id"];
+
+			$stt = $db->prepare('INSERT INTO Orders(address, user_id) VALUES (:address :userId);');
+			$stt->bindValue(':userId', $userId, PDO::PARAM_STR);
+			$stt->bindValue(':address', $addressId, PDO::PARAM_STR);
+			$stt->execute();
+
+
+		}
+		else{
+			echo "Error: User not found<br>";
+		}
+	  	
+
 	}
 	catch (PDOException $ex)
 	{
@@ -31,31 +59,6 @@
 	  die();
 	}
 
-	echo $sessionUser . ' ' . $adress . $zip . $state;
-
-	//check to see if the user_id matches the session id of the user
-	$stmt = $db->prepare('SELECT * FROM Customer WHERE userName=:sessionUser');
-	$stmt->bindValue(':sessionUser', $sessionUser, PDO::PARAM_STR);
-	$stmt->execute();
-	$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-	//Add the user_id into the Order table
-	if($rows[0]["username"])
-	{
-		echo $rows[0]["username"] ." " . $rows[0]["user_id"];
-		$userId = $rows[0]["user_id"];
-
-		$stt = $db->prepare('INSERT INTO Orders(address, user_id) VALUES (:address :userId);');
-		$stt->bindValue(':userId', $userId, PDO::PARAM_STR);
-		$stt->bindValue(':address', $addressId, PDO::PARAM_STR);
-		$stt->execute();
-
-
-	}
-	else{
-		echo "Error: User not found<br>";
-	}
 
 	
 
