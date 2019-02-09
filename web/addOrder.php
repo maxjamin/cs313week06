125744d69e8ae852b75ef0d4bb08d2660ea1a603
maxjamin <maxjamin@gmail.com>
2019-02-09 11:40:34 -0700

    add user button

--- web/addOrder.php
+++ web/addOrder.php
@@ -40,8 +40,8 @@
 			echo $rows[0]["username"] ." " . $rows[0]["user_id"];
 			$userId = $rows[0]["user_id"];
 
-			$stt = $db->prepare('INSERT INTO Order(address, user_id) VALUES (:
-				address, :userId);');
+			$stt = $db->prepare('INSERT INTO Order(address, user_id) VALUES (
+				:address, :userId);');
 			$stt->bindValue(':address', $addressId, PDO::PARAM_STR);
 			$stt->bindValue(':userId', $userId, PDO::PARAM_INT);
 			$stt->execute();
