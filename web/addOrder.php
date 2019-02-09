87c8f4b1d97507b0841b2dab709a0f0632604558
maxjamin <maxjamin@gmail.com>
2019-02-09 11:20:19 -0700

    add user button

--- web/addOrder.php
+++ web/addOrder.php
@@ -6,7 +6,9 @@
 	$adress = htmlspecialchars($_POST['addressEntered']);
 	$zip = htmlspecialchars($_POST['zipEntered']);
 	$state = htmlspecialchars($_POST['stateEntered']);
+
 	$sessionUser = $_SESSION["sessionUserName"];
+	$addressId = $address . " " . $zip . " " . $state;
 
 	try
 	{
@@ -31,13 +33,31 @@
 
 	echo $sessionUser . ' ' . $adress . $zip . $state;
 
-
+	//check to see if the user_id matches the session id of the user
 	$stmt = $db->prepare('SELECT * FROM Customer WHERE userName=:sessionUser');
 	$stmt->bindValue(':sessionUser', $sessionUser, PDO::PARAM_STR);
 	$stmt->execute();
 	$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-	echo $rows[0]["username"] ." " . $rows[0]["user_id"];
+
+	//Add the user_id into the Order table
+	if($rows[0]["username"])
+	{
+		echo $rows[0]["username"] ." " . $rows[0]["user_id"];
+		$userId = $rows[0]["user_id"];
+
+		$stmt = $db->prepare('INSERT INTO Orders(address, user_id) VALUES (:address :userId);');
+		$stmt->bindValue(':userId', $userId, PDO::PARAM_STR);
+		$stmt->bindValue(':address', $addressId, PDO::PARAM_STR);
+		$stmt->execute();
+
+
+	}
+	else{
+		echo "Error: User not found<br>";
+	}
+
+	
 
 
 ?>
